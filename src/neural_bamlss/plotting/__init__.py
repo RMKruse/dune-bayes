ad9ad8473abd5e03749ebc42c5e2373b37f54fa5
RMKruse <rene.marcel.kruse@protonmail.com>
2026-06-03 12:47:32 +0200

    Export predictive_quantiles and plot_dist from plotting package (issue 0008 / GitHub #9)

--- src/neural_bamlss/plotting/__init__.py
+++ src/neural_bamlss/plotting/__init__.py
@@ -1,5 +1,11 @@
-"""Plotting utilities for neural-bamlss (issue 0006 / GitHub #7)."""
+"""Plotting utilities for neural-bamlss (issues 0006, 0008 / GitHub #7, #9)."""
 
 from neural_bamlss.plotting.effect_ribbon import plot_effect_ribbon, ribbon_quantiles
+from neural_bamlss.plotting.response_band import plot_dist, predictive_quantiles
 
-__all__ = ["ribbon_quantiles", "plot_effect_ribbon"]
+__all__ = [
+    "ribbon_quantiles",
+    "plot_effect_ribbon",
+    "predictive_quantiles",
+    "plot_dist",
+]
