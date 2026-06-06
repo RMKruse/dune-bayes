2c1aa0dcb413910a594f2a981684907442c119dd
RMKruse <rene.marcel.kruse@protonmail.com>
2026-06-06 19:07:43 +0200

    Drop InteractionTerm from formula exports

--- src/neural_bamlss/formula/__init__.py
+++ src/neural_bamlss/formula/__init__.py
@@ -1,11 +1,10 @@
 """Formula-string surface for neural-bamlss (issue 0016–0017 / GitHub #37, #41)."""
 
 from neural_bamlss.formula.parser import (
-    InteractionTerm,
     ParsedFormula,
     Term,
     build_formula,
     parse_formula,
 )
 
-__all__ = ["InteractionTerm", "ParsedFormula", "Term", "build_formula", "parse_formula"]
+__all__ = ["ParsedFormula", "Term", "build_formula", "parse_formula"]
