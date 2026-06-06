c399e2ec57e64e81b26ded843628b24232dfd1b5
RMKruse <rene.marcel.kruse@protonmail.com>
2026-06-06 20:45:45 +0200

    Narrow validate_args assert to ValueError in Gamma test

--- tests/families/test_gamma_family.py
+++ tests/families/test_gamma_family.py
@@ -55,5 +55,5 @@ class TestGammaFamilyContract:
         family_strict = GammaFamily(validate_args=True)
         params = torch.tensor([[1.0, 1.0]])
         y_neg = torch.tensor([-1.0])
-        with pytest.raises(Exception):
+        with pytest.raises(ValueError):
             family_strict.log_prob(params, y_neg)
